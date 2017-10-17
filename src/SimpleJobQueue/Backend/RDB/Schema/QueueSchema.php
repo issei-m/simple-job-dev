032794acd285cfd85f42a78e077c66097f9d25f7
Issei.M <issei.m7@gmail.com>
2017-10-18 07:15:43 +0900

    [SimpleJobQueue] Make rdb schema class immutable

--- src/SimpleJobQueue/Backend/RDB/Schema/QueueSchema.php
+++ src/SimpleJobQueue/Backend/RDB/Schema/QueueSchema.php
@@ -5,15 +5,27 @@ declare(strict_types=1);
 namespace Issei\SimpleJobQueue\Backend\RDB\Schema;
 
 /**
+ * @property-read string $table
+ * @property-read string $idColumn
+ * @property-read string $nameColumn
+ * @property-read string $argumentsColumn
+ * @property-read string $executeAtColumn
+ * @property-read string $maxRetriesColumn
+ * @property-read string $retryIntervalColumn
+ *
  * @author Issei Murasawa <issei.m7@gmail.com>
  */
 final class QueueSchema
 {
-    public $table = 'jobs';
-    public $idColumn = 'id';
-    public $nameColumn = 'name';
-    public $argumentsColumn = 'arguments';
-    public $executeAtColumn = 'execute_at';
-    public $maxRetriesColumn = 'max_retries';
-    public $retryIntervalColumn = 'retry_interval';
+    use ReadOnlyPropertiesTrait;
+
+    private const DEFAULT_PROPERTIES = [
+        'table' => 'jobs',
+        'idColumn' => 'id',
+        'nameColumn' => 'name',
+        'argumentsColumn' => 'arguments',
+        'executeAtColumn' => 'execute_at',
+        'maxRetriesColumn' => 'max_retries',
+        'retryIntervalColumn' => 'retry_interval',
+    ];
 }
--- src/SimpleJobQueue/Backend/RDB/Schema/ReporterSchema.php
+++ src/SimpleJobQueue/Backend/RDB/Schema/ReporterSchema.php
@@ -5,18 +5,33 @@ declare(strict_types=1);
 namespace Issei\SimpleJobQueue\Backend\RDB\Schema;
 
 /**
+ * @property-read string $table
+ * @property-read string $jobIdColumn
+ * @property-read string $stateColumn
+ * @property-read string $workerNameColumn
+ * @property-read string $startedAtColumn
+ * @property-read string $finishedAtColumn
+ * @property-read string $exitCodeColumn
+ * @property-read string $stdoutColumn
+ * @property-read string $stderrColumn
+ * @property-read string $retryToColumn
+ *
  * @author Issei Murasawa <issei.m7@gmail.com>
  */
 final class ReporterSchema
 {
-    public $table = 'job_reports';
-    public $jobIdColumn = 'job_id';
-    public $stateColumn = 'state';
-    public $workerNameColumn = 'worker_name';
-    public $startedAtColumn = 'started_at';
-    public $finishedAtColumn = 'finished_at';
-    public $exitCodeColumn = 'exit_code';
-    public $stdoutColumn = 'stdout';
-    public $stderrColumn = 'stderr';
-    public $retryToColumn = 'retry_to';
+    use ReadOnlyPropertiesTrait;
+
+    private const DEFAULT_PROPERTIES = [
+        'table' => 'job_reports',
+        'jobIdColumn' => 'job_id',
+        'stateColumn' => 'state',
+        'workerNameColumn' => 'worker_name',
+        'startedAtColumn' => 'started_at',
+        'finishedAtColumn' => 'finished_at',
+        'exitCodeColumn' => 'exit_code',
+        'stdoutColumn' => 'stdout',
+        'stderrColumn' => 'stderr',
+        'retryToColumn' => 'retry_to',
+    ];
 }
